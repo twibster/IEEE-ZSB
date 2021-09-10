c30b37c448d3ec00099ec0a62313f8078fd529b1
Omar Omran <omaromran2003@gmail.com>
2021-09-11 03:11:50 +0400

    separate email config file

---
+++ website/email_config.py
@@ -0,0 +1,8 @@
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'email_here',
+    "MAIL_PASSWORD": 'email_here'
+}
