3cdd11a44b157222fa2e54adbe24c3d2066820e0
Omar Omran <omaromran2003@gmail.com>
2021-09-18 04:42:55 +0400

    use environment variable as the database url

--- website/__init__.py
+++ website/__init__.py
@@ -6,9 +6,18 @@ from website.email_config import mail_settings
 import os
 
 app = Flask(__name__)
+
+def database_url_handler(url):
+    ''' this function checks for an outdated postgresql url as it is not supported by sql-alchemy anymore'''
+    if 'postgres://' in url:
+        url.replace("postgres://", "postgresql://", 1)
+
+    return url
+    
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url_handler(os.environ.get('DATABASE_URL'))
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['GOOGLE_API']= os.environ.get('GOOGLE_API')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 app.config['TASKS_FILE_DOWNLOAD'] = os.path.join('media/tasks')
 app.config['SUBMITS_FILE_DOWNLOAD'] = os.path.join('media','submits')
 app.config['TASKS_FILE'] = os.path.join(app.root_path,'media/tasks')
