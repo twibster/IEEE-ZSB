20ca84f17cffb1e0606eb042879c18915e0ca823
Omar Omran <omaromran2003@gmail.com>
2021-09-26 10:52:36 +0400

    styles: fix live search result inconsistent width

--- website/static/scripts/navbar.js
+++ website/static/scripts/navbar.js
@@ -1,7 +1,14 @@
 $(function() {
   $(document).click(function (event) {
     container=$(".navbar-collapse")
+
+    //set result width equal to the search fiedl width
+    if ((container.is(":visible")) && (result.offsetWidth != search.offsetWidth)) 
+      search_div.style.width ='100%'
+      result.style.width = search.offsetWidth;
+      
+    //hide the navbar when user cliks outside of the content of the navbar
     if (!container.is(event.target) && !container.has(event.target).length)
-      container.collapse('hide');
+      container.collapse('hide'); 
   });
 });
