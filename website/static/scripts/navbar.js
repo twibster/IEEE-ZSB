1969da58e0d468252ac0ffad26a4ca20d2ecd0d7
Omar Omran <omaromran2003@gmail.com>
2021-09-26 13:23:46 +0400

    styles: fix search div inconsistent width

--- website/static/scripts/navbar.js
+++ website/static/scripts/navbar.js
@@ -1,14 +1,14 @@
 $(function() {
   $(document).click(function (event) {
-    container=$(".navbar-collapse")
+    var navbar=$(".navbar-collapse")
 
     //set result width equal to the search fiedl width
-    if ((container.is(":visible")) && (result.offsetWidth != search.offsetWidth)) 
+    if ((navbar.is(":visible")) && (result.offsetWidth != search.offsetWidth) && (isMobile))
       search_div.style.width ='100%'
       result.style.width = search.offsetWidth;
       
     //hide the navbar when user cliks outside of the content of the navbar
-    if (!container.is(event.target) && !container.has(event.target).length)
-      container.collapse('hide'); 
+    if (!navbar.is(event.target) && !navbar.has(event.target).length && (isMobile))
+      navbar.collapse('hide'); 
   });
 });
