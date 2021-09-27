ee034c9e745324224634da92bcfe7b5a4fc3d7fd
Omar Omran <omaromran2003@gmail.com>
2021-09-27 20:27:30 +0400

    update department URL in live search results

--- website/static/scripts/live_search.js
+++ website/static/scripts/live_search.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         success:function(result){
            $.each(result, function(key, value){ //loop through the query
               if (key.indexOf('__d__')!= -1){  //if the key of the record is a department
-                $('#result').append('<a class="no-dec" href="/'+value.department+'"><li class="list-group-item link-class">'+value.department+' | <span class="text-muted">'+value.tasks+'</span></li></a>');
+                $('#result').append('<a class="no-dec" href="/department/'+value.department+'"><li class="list-group-item link-class">'+value.department+' | <span class="text-muted">'+value.tasks+'</span></li></a>');
               }
               else{ //if the key of the record is a user
                 $('#result').append('<a class="no-dec" href="/profile/'+value.username+'"><li class="list-group-item link-class"><img src="/static/profile_pics/'+value.image_file+'"class="rounded-circle noti-img" />'+value.first_name+" "+value.last_name+' | <span class="text-muted">'+value.position+'</span></li></a>');
